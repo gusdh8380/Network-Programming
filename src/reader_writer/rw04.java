553d251298aeaab1436e50a9ac95df936f6713ae
jeon, Hyeon O <gusdh2510@gmail.com>
2023-10-18 02:09:11 +0900

    reader&writer - web에서 다운로드

---
+++ src/reader_writer/rw04.java
@@ -0,0 +1,28 @@
+package reader_writer;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.io.Reader;
+import java.net.HttpURLConnection;
+import java.net.URL;
+
+public class rw04 {
+	public static void main(String[] args) throws IOException {
+		var url = new URL("https://www.skhu.ac.kr");
+		var connection = (HttpURLConnection) url.openConnection();
+		InputStream in = connection.getInputStream();
+
+
+
+		Reader reader = new InputStreamReader(in);
+		while (true) {
+			int ch = reader.read();
+			if (ch < 0)
+				break;
+			System.out.print((char) ch);
+		}
+		reader.close();
+	}
+
+}
