a5b241cf2c9b5934a4b006bf39dc0ce88c43309a
jeon, Hyeon O <gusdh2510@gmail.com>
2023-10-18 02:56:23 +0900

    자원할당구현 - 추가코드 주

--- src/zipOutputStream/Ex02.java
+++ src/zipOutputStream/Ex02.java
@@ -17,10 +17,10 @@ public class Ex02 {
 		HttpURLConnection connection = null;
 		InputStream in = null;
 		OutputStream out = null;
-		//반납해야하는 변수를 null로 초기
+		// 반납해야하는 변수를 null로 초기
 
 		try {
-			//자원 할당
+			// 자원 할당
 			connection = (HttpURLConnection) urlObj.openConnection();
 			in = new BufferedInputStream(connection.getInputStream());
 			out = new BufferedOutputStream(new FileOutputStream(path));
@@ -31,8 +31,8 @@ public class Ex02 {
 				out.write(b);
 			}
 		} finally {
-			//자원이 할당된 변수의 값은 null이 아님
-			//아래코드로 자원 반납코드가 모든 상황에서 실행된다.
+			// 자원이 할당된 변수의 값은 null이 아님
+			// 아래코드로 자원 반납코드가 모든 상황에서 실행된다.
 			if (in != null)
 				in.close();
 			if (out != null)
@@ -41,6 +41,23 @@ public class Ex02 {
 				connection.disconnect();
 		}
 	}
+	/* 아래와 같이 구현 가능(try with resource 문법)
+
+	 static void download(String url, String path) throws IOException {
+	  var urlObj = new URL(url);
+	  var connection = (HttpURLConnection)urlObj.openConnection();
+
+	 try (
+	  InputStream in = new BufferedInputStream(connection.getInputStream());
+	  OutputStream out = new BufferedOutputStream(new FileOutputStream(path));
+	   ){
+	 	while (true) {
+	 	 int b = in.read();
+	 	  if (b < 0) break;
+	 	   out.write(b);
+	 	}
+	 	connection.disconnect(); } }
+	 */
 
 	public static void main(String[] args) throws IOException {
 		File directory = new File("zip_test");
