e8f68269e8504b49544849a2dd856e2821b9b144
jeon, Hyeon O <gusdh2510@gmail.com>
2023-10-30 12:59:43 +0900

    한글 문자열 전송의 경

---
+++ src/socket/DaytimeClient2.java
@@ -0,0 +1,25 @@
+package socket;
+
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.Reader;
+import java.net.Socket;
+import java.net.UnknownHostException;
+
+public class DaytimeClient2 {
+	public static void main(String[] args) throws UnknownHostException, IOException {
+		final String HOST = "localhost";
+		final int PORT = 13;
+		try (Socket socket = new Socket(HOST, PORT)) {
+			StringBuilder result = new StringBuilder();
+			Reader reader = new InputStreamReader(socket.getInputStream(), "UTF-8");
+			while (true) {
+				int c = reader.read();
+				if (c == -1)
+					break;
+				result.append((char) c);
+			}
+			System.out.println(result.toString());
+		}
+	}
+}
---
+++ src/socket/DaytimeServer2.java
@@ -0,0 +1,29 @@
+package socket;
+
+import java.io.IOException;
+import java.io.OutputStreamWriter;
+import java.io.Writer;
+import java.net.ServerSocket;
+import java.net.Socket;
+import java.util.Date;
+
+public class DaytimeServer2 {
+	public static void main(String[] args) {
+		final int PORT = 13;
+		try (ServerSocket serverSocket = new ServerSocket(PORT)) {
+			while (true) {
+				try (Socket socket = serverSocket.accept()) {
+					Writer out = new OutputStreamWriter(socket.getOutputStream(), "UTF-8");
+					Date now = new Date();
+					out.write("현재 시각: ");
+					out.write(now.toString());
+					out.write("\r\n");
+					out.flush();
+				} catch (IOException ex) {
+				}
+			}
+		} catch (IOException ex) {
+			System.err.println(ex);
+		}
+	}
+}
