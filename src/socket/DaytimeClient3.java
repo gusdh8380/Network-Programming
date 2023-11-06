02388e9ea14c45ca3a53169895bf904926ded22f
jeon, Hyeon O <gusdh2510@gmail.com>
2023-11-06 09:23:29 +0900

    ObjectOutputStream 클래스의 메소드로 출력된 내용을 읽으려면,
    ObjectInputStream 클래스가 필요

--- src/socket/DaytimeClient3.java
+++ src/socket/DaytimeClient3.java
@@ -1,5 +1,17 @@
 package socket;
 
-public class DaytimeClient3 {
+import java.io.ObjectInputStream;
+import java.net.Socket;
+import java.util.Date;
 
+public class DaytimeClient3 {
+	public static void main(String[] args) throws Exception {
+		final String HOST = "localhost";
+		final int PORT = 13;
+		try (Socket socket = new Socket(HOST, PORT)) {
+			ObjectInputStream in = new ObjectInputStream(socket.getInputStream());
+			Date dt = (Date) in.readObject();
+			System.out.println(dt);
+		}
+	}
 }
