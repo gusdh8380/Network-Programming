686c8f85d1e254bcc2c035527874392ae7055a3e
Jeon, HyeonO <gusdh2510@gmail.com>
2023-12-11 06:14:31 +0900

    서버에서 클라로 객체 전송

---
+++ src/Exercise/Exam5Client.java
@@ -0,0 +1,34 @@
+package Exercise;
+
+import java.io.ObjectInputStream;
+import java.net.Socket;
+
+// 객체타입을 주고 받는 서버과 클라 구현
+public class Exam5Client {
+
+
+	static class GuguTask implements Runnable {
+		@Override
+		public void run() {
+			final String HOST = "localhost";
+			final int PORT = 10015;
+			try (Socket socket = new Socket(HOST, PORT)) {
+				ObjectInputStream in = new ObjectInputStream(socket.getInputStream());
+
+
+
+					DTO d = (DTO)in.readObject();
+					System.out.printf("%s", d.value1);
+
+			} catch (Exception e) {
+				e.printStackTrace();
+			}
+		}
+	}
+
+	public static void main(String[] args) {
+
+			new Thread(new GuguTask()).start();
+	}
+
+}
