29696ae7ed3ecc21cdd970559960e76dcd1a2257
Jeon, HyeonO <gusdh2510@gmail.com>
2023-12-11 06:14:57 +0900

    서버에서 클라로 객체 전송, 멀티스레드로

---
+++ src/Exercise/Exam6Server.java
@@ -0,0 +1,50 @@
+package Exercise;
+
+import java.io.IOException;
+import java.io.ObjectOutputStream;
+import java.net.ServerSocket;
+import java.net.Socket;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+
+
+//객체를 주고 받는 서버와 클라 구
+public class Exam6Server {
+    public static void main(String[] args) {
+        final int PORT = 10015;
+        try (ServerSocket serverSocket = new ServerSocket(PORT)) {
+            ExecutorService executor = Executors.newFixedThreadPool(200);
+            while (true) {
+                try {
+                    Socket socket = serverSocket.accept();
+                    executor.submit(new GuguTask(socket));
+                } catch (IOException ex) {
+                    System.err.println("Socket Accept Error: " + ex);
+                }
+            }
+        } catch (IOException ex) {
+            System.err.println("ServerSocket Error: " + ex);
+        }
+    }
+
+    static class GuguTask implements Runnable {
+        Socket socket;
+
+        public GuguTask(Socket socket) {
+            this.socket = socket;
+        }
+
+        @Override
+        public void run() {
+            try (Socket autoClose = socket) {
+                ObjectOutputStream out = new ObjectOutputStream(socket.getOutputStream());
+                String a = "전현오";
+                DTO d = new DTO(a);
+                out.writeObject(d);
+                out.flush();
+            } catch (IOException ex) {
+                System.err.println("GuguTask Error: " + ex);
+            }
+        }
+    }
+}
